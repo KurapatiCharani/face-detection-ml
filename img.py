b2dfac43fa94901af150b77cc8940f5f157a86cb
KurapatiCharani <125110729+KurapatiCharani@users.noreply.github.com>
2024-03-09 13:37:34 +0530

    fifth commit
    Date: 09-03-2024

---
+++ img.py
@@ -0,0 +1,20 @@
+import cv2
+cascade_classifier = cv2.CascadeClassifier('E:\\face-rec\\haarcascade_frontalface_default.xml')
+#cascade_classifier = cv2.CascadeClassifier('E:\\face-rec\\haarcascade_eye.xml')
+#cascade_classifier = cv2.CascadeClassifier('E:\\face-rec\\haarcascade_smile.xml')
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, 0)
+    detections = cascade_classifier.detectMultiScale(gray,scaleFactor=1.3,minNeighbors=5)
+    if(len(detections) > 0):
+        (x,y,w,h) = detections[0]
+        frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,65,98),2)
+
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+
+cap.release()
+cv2.destroyAllWindows()
